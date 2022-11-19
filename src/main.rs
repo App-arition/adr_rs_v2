2c67f05af2465cc6c0a30efd38f128fb03ff595e
adriel lopez <adriel.v.lopez@gmail.com>
2022-11-19 15:09:29 -0600

    Adding initial Rust sandbox files

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    greeting();
+
+    let adrint = 2;
+    println!("{adrint}");
+}
+
+
+// Functions
+fn greeting() {
+    println!("Hello, world!");
+    println!("This is the sandbox environment for Adriel L's Rust development.");
+}
